4cca125b282e62725ecb52f09b9334563eccb33c
Jonathan <joosoriore@unal.edu.co>
2024-02-19 22:15:32 -0500

    segundo py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 class Asiento:
 
-    def _init_(self, color, precio, registro):
+    def __init__(self, color, precio, registro):
 
         self.color = color
         self.precio = precio
@@ -14,7 +14,7 @@ class Asiento:
 
 class Motor:
 
-    def _init_(self, numeroCilindros, tipo, registro):
+    def __init__(self, numeroCilindros, tipo, registro):
 
         self.numeroCilindros = numeroCilindros
         self.tipo = tipo
@@ -34,7 +34,7 @@ class Auto:
 
     cantidadCreados = 0
 
-    def _init_(self, modelo, precio, asientos, marca, motor, registro):
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
 
         self.modelo = modelo
         self.precio = precio
